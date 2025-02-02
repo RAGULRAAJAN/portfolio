1726ab09278c6c6c873892de02da4d6559ac4a1d
RAGUL RAAJAN THIRUPATHI <ragul18012006@gmail.com>
2025-02-02 23:01:42 +0530

    deploy config

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,11 @@ import { ThemeProvider } from './common/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     
+
     <ThemeProvider>
       <App />
     </ThemeProvider>
-   
-   
+
+
   </React.StrictMode>
 );
